6e7b2609baaf748c9254034bfea3d6245b706093
Roy Collings <roy.collings@gmail.com>
2023-03-27 20:15:04 +0200

    wip

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str):
+async def read_item(item_id: int):
     return {"item_id": item_id}
 
 @app.get("/")
