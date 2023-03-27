718954616b08d2c04b1e5c789ba7d3f503a25d1e
Roy Collings <roy.collings@gmail.com>
2023-03-27 20:47:50 +0200

    wip

--- generate_openapi.py
+++ generate_openapi.py
@@ -7,7 +7,8 @@ with open('oas/openapi.json', 'w') as f:
     json.dump(get_openapi(
         title=app.title,
         version=app.version,
-        openapi_version=app.openapi_version,
+        # openapi_version=app.openapi_version,
+        openapi_version="swagger:2.0",
         description=app.description,
         routes=app.routes,
     ), f)
